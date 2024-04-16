7721fc37e8a1f4dea47cb5f1d78605b8fb040539
Sudil2004 <119151831+Sudil2004@users.noreply.github.com>
2024-04-16 07:24:21 +0545

    Updated Login

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,31 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './login.css';
 import { GoogleLogin } from 'react-google-login';
-
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContexts';
+import { Alert } from 'react-bootstrap';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
   const [showWelcome, setShowWelcome] = useState(false);
 
+  const { login } = useAuth();
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const navigate = useNavigate()
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError('');
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      navigate("/", {replace:true})
+      setLoggedIn(true);
+      alert('Login successful!');
+    } catch (error) {
+      setError('Failed to log in');
+    }
+
+    setLoading(false);
+  }
+
   useEffect(() => {
-    // Show the "Welcome back" text when the tab is opened
     setShowWelcome(true);
   }, []);
 
-  const handleLogin = () => {
-    if (username === 'user' && password === 'password') {
-      setLoggedIn(true);
-      alert('Login successful!');
-    } else {
-      alert('Invalid username or password');
-    }
-  };
-
   const handleLogout = () => {
     setLoggedIn(false);
-    setUsername('');
+    setEmail('');
     setPassword('');
   };
 
@@ -42,56 +60,61 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="container-login">
-      <div className="content">
-        {showWelcome && (
-          <div className="welcome-text">
-            <h3>Welcome back !</h3>
-          </div>
-        )}
-        <div className="form">
-          <div className="login-container">
-            {loggedIn ? (
-              <div>
-                <h2>Welcome, {username}!</h2>
-                <button onClick={handleLogout}>Logout</button>
-              </div>
-            ) : (
-              <div>
-                <h2>Login</h2>
-                <input
-                  type="text"
-                  placeholder="Username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-                <br />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <br />
-                <p onClick={handleForgotPassword} className="forgot-password">
-                  Forgot Password?
-                </p>
-                <button onClick={handleLogin}>Login</button>
-                <GoogleLogin
-                  clientId="548860831968-2ic30h94q04hi5nk3v2oid5hjvuvvudv.apps.googleusercontent.com"
-                  buttonText="Continue with Google"
-                  onSuccess={responseGoogleSuccess}
-                  onFailure={responseGoogleFailure}
-                  cookiePolicy={'single_host_origin'}
-                />
-              </div>
-              
-            )}
+    <form onSubmit={handleSubmit}>
+      <div className="container-login">
+        <div className="content">
+          {showWelcome && (
+            <div className="welcome-text">
+              <h3>Welcome back!</h3>
+              {error && <Alert variant='danger'>{error}</Alert>}
+            </div>
+          )}
+          <div className="form">
+            <div className="login-container">
+              {loggedIn ? (
+                <div>
+                  <h2>Welcome, {email}!</h2>
+                  <button onClick={handleLogout}>Logout</button>
+                </div>
+              ) : (
+                <div>
+                  <h2>Login</h2>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    ref={emailRef}
+                    required
+                  />
+                  <br />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    ref={passwordRef}
+                    required
+                  />
+                  <br />
+                  <div className="sign">Need an account? <Link to="/signup">Sign up</Link></div>
+                  <p onClick={handleForgotPassword} className="forgot-password"><Link to="/forgot-password">Forgot Password</Link>
+                  </p>
+                  <button disabled={loading}>Login</button>
+                  <GoogleLogin
+                    clientId="548860831968-2ic30h94q04hi5nk3v2oid5hjvuvvudv.apps.googleusercontent.com"
+                    buttonText="Continue with Google"
+                    onSuccess={responseGoogleSuccess}
+                    onFailure={responseGoogleFailure}
+                    cookiePolicy={'single_host_origin'}
+                  />
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    
+    </form>
   );
 };
 
