10f3389b38d4730ad6cf6b56263e50b862b61977
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2024-05-14 21:53:04 +0545

    Edited About-section and updated the footer section

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ import ScrollToTop from "./config/ScrollToTop";
 import Header from "./components/header";
 import Footer from './components/footer';
 
-import Home from './pages/home';
+import Home from './pages/Home';
 
 import Login from './pages/auth/login';
 import Signup from './pages/auth/signup';
